5b3eada5d1cf627eaf202d2ccbc49f318b3756fe
magpie <maggie@pierceclan.net>
2020-10-12 19:37:15 -0400

    package.json updates; http req in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
 
-export default function Home() {
-  return (
-    <div>Welcome!</div>
-  )
+export default function Home() 
+{
+  request();
+  return (<div>Welcome!</div>);
+}
+
+async function request()
+{
+  const url = "http://localhost:3000/api/menu"
+  const response = await axios.get(url);
+  console.log(response.data);
 }
