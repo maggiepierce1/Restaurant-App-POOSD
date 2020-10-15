23c6cc0a76e8ee0f585db8fdd0a613ca70845167
magpie <maggie@pierceclan.net>
2020-10-15 17:09:09 -0400

    finally got the stupid button working

--- pages/menu.js
+++ pages/menu.js
@@ -1,10 +1,18 @@
-import { List, Header, Grid, Menu, Container } from 'semantic-ui-react'
+import { Header, Grid, Menu, Popup, Button, Divider } from 'semantic-ui-react'
 import axios from 'axios'
+
+
 
 function MenuPage({ menuItems })
 {
+    function handleClick(e)
+    {
+        alert(e.currentTarget.value);
+        // addToCart(e.currentTarget.value);
+        e.preventDefault();
+    }
     return (<>
-        <Header color = "teal" textAlign = "center">
+        <Header color = "teal" textAlign = "center" inverted>
             This is the menu.
         </Header>
         <Grid columns={3} divided textAlign = "center" verticalAlign = "middle">
@@ -12,8 +20,13 @@ function MenuPage({ menuItems })
             <Grid.Column>
                 APPETIZERS
                 <Menu vertical fluid>
-                {menuItems.map((menuItem) => (
-                <Menu.Item>{menuItem.name}. . .{menuItem.price}</Menu.Item>
+                    {menuItems.map((menuItem) => (
+                    <Menu.Item>{menuItem.name}. . .{menuItem.price}
+                        <Popup
+                        trigger={<Button value = {menuItem.name} icon='add' compact onClick = {handleClick}/>}
+                        content="Click here to add this item to your cart"
+                        basic/>
+                    </Menu.Item>
                 ))}
                 </Menu>
             </Grid.Column>
@@ -21,7 +34,12 @@ function MenuPage({ menuItems })
                 ENTREES
                 <Menu vertical fluid>
                 {menuItems.map((menuItem) => (
-                <Menu.Item>{menuItem.name}. . .{menuItem.price}</Menu.Item>
+                <Menu.Item>{menuItem.name}. . .{menuItem.price}
+                    <Popup
+                    trigger={<Button value = {menuItem.name} icon='add' compact onClick = {handleClick}/>}
+                    content="Click here to add this item to your cart"
+                    basic/>
+                </Menu.Item>
                  ))}
                 </Menu>
             </Grid.Column>
@@ -29,25 +47,18 @@ function MenuPage({ menuItems })
                 DESSERTS
                 <Menu vertical fluid>
                 {menuItems.map((menuItem) => (
-                <Menu.Item>{menuItem.name}. . .{menuItem.price}</Menu.Item>
+                <Menu.Item>{menuItem.name}. . .{menuItem.price}
+                    <Popup
+                    trigger={<Button value = {menuItem.name} icon='add' compact onClick = {handleClick}/>}
+                    content="Click here to add this item to your cart"
+                    basic/>
+                </Menu.Item>
                  ))}
                 </Menu>
             </Grid.Column>
           </Grid.Row>
         </Grid>
       </>);
-      /*
-    return (<>
-        <Header textAlign = "center">
-            This is the menu.
-        </Header>
-        <List>
-        {menuItems.map((menuItem) => (
-            <List.Item>{menuItem.name}. . .{menuItem.price}</List.Item>
-        ))}
-        </List>
-      </>);
-      */
 }
 
 export async function getStaticProps()
@@ -55,8 +66,14 @@ export async function getStaticProps()
   const url = "http://localhost:3000/api/menu"
   const response = await axios.get(url);
   const menuItems = response.data;
-  console.log(menuItems);
   return {props : { menuItems } };
 }
+/*
 
+export async function addToCart(name)
+{
+    const url = "http://localhost:3000/api/cart"
+    const response = await axios.post(url, { name : {name}});
+}
+*/
 export default MenuPage;
