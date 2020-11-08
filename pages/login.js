0d8d54bd3cc4047d994a4f8d5b0ed36a4f611595
magpie <maggie@pierceclan.net>
2020-11-07 22:26:20 -0500

    improvements to login page UI

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import { Form, Container, Header } from 'semantic-ui-react'
+import { Form, Container, Header, Grid, Segment, Button } from 'semantic-ui-react'
 import Link from 'next/link'
 import 'semantic-ui-css/semantic.min.css'
 import Router from 'next/router'
@@ -36,28 +36,31 @@ class Login extends React.Component
   render() 
   {
     return (<>
-    <Header textAlign = "center">
-        Log in to your account:
-    </Header>
-    <Container>
-        <Form>
+    <Grid style={{ height: '100vh' }} textAlign = "center" verticalAlign = "middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as = "h2" size = "large" textAlign = "center">
+            Log in to your account:
+        </Header>
+        <Form size = 'large'>
+          <Segment stacked>
             <Form.Field>
-            <label>Username</label>
-            <input type = "text" ref = {this.username} placeholder = 'Username'/>
+            <input type = "text" ref = {this.username} placeholder = 'E-mail Address'/>
             </Form.Field>
             <Form.Field>
-            <label>Password</label>
             <input type = "text" ref = {this.password} placeholder = 'Password'/>
             </Form.Field>
-            <Form.Button onClick = {this.loginAsCustomer}>
-                Log in as customer
-            </Form.Button>
-            <Form.Button onClick = {this.loginAsEmployee}> 
-                Log in as employee
-            </Form.Button>
-
+            <Segment.Inline>
+              <Button color = 'blue' onClick = {this.loginAsCustomer}>
+                  Log in as customer
+              </Button>
+              <Button color = 'green' onClick = {this.loginAsEmployee}> 
+                  Log in as employee
+              </Button>
+            </Segment.Inline>
+          </Segment>
         </Form>
-    </Container>
+      </Grid.Column>
+    </Grid>
     </>);
   }
 }
