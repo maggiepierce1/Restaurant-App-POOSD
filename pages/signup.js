8168778e2db91fff9d1e97eadf11d3f0a21f6ee1
magpie <maggie@pierceclan.net>
2020-11-27 13:10:26 -0500

    signup page routing error fixed

--- pages/signup.js
+++ pages/signup.js
@@ -35,7 +35,7 @@ class Signup extends React.Component
     }
 
     if (successfulSignup)
-      Router.push('/index');
+      Router.push('/');
     else
       alert("There is already an account associated with this e-mail address. Please try again, or return to login.");
     
